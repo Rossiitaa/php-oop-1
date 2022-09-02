b7b01a0ccb824a606683e62764c11bdc6dc6e4b0
Rossiitaa <rosario.scuotto@outlook.it>
2022-09-02 15:39:41 +0200

    added classes to movies

---
+++ classes/movies.php
@@ -0,0 +1,28 @@
+<?php 
+    class Movie {
+        private $title;
+        private $director;
+        private $year;
+
+        public function __construct($_title, $_director, $_year) {
+            $this->title = $_title;
+            $this->director = $_director;
+            $this->year = $_year;
+        }
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+    public function getDirector() {
+        return $this->director;
+    }
+    public function getYear() {
+        return $this->year;
+    }
+
+
+
+
+
+?>
