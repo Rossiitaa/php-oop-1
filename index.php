5f9d873e982765a5b5f470787ef8f7548c23c767
Rossiitaa <rosario.scuotto@outlook.it>
2022-09-02 15:55:22 +0200

    added the instances to movies

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+    require_once __DIR__ . '/classes/movies.php';
+
+    $movie1 = new Movie('The Godfather', 'Francis Ford Coppola', 1972);
+    $movie2 = new Movie('The Shawshank Redemption', 'Frank Darabont', 1994);
+    $movie3 = new Movie('Natale in India', 'Neri Parenti', 2003);
+    $movie4 = new Movie('Limitless', 'Neil Burger', 2011);
+    $movie5 = new Movie('Call me by your name', 'Luca Guadagnino', 2017);
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
